8a26d51a42bca414aec0ad56066326781e704a36
Andy Sherwood <sherwooda@cleanwaterservices.org>
2013-04-29 16:49:27 -0700

    Build script is work. nuget package deployed to intranet

--- build.ps1
+++ build.ps1
@@ -2,5 +2,5 @@ if (test-path .\build) { ri -r -fo .\build }
 mkdir .\build | out-null
 .\tools\psake\psake.ps1 default.ps1 default 3.5
 .\tools\psake\psake.ps1 default.ps1 default 4.0
-.\Tools\nuget\NuGet.exe pack .\CWS.Utility.SharePoint.nuspec -BasePath .\build -OutputDirectory .\build
+.\Tools\nuget\NuGet.exe pack .\Amazon.WebServices.MechanicalTurk.nuspec -BasePath .\build -OutputDirectory .\build
 copy .\build\*.nupkg "\\fileserv\DistSW\Clean Water Services\Packages"
--- default.ps1
+++ default.ps1
@@ -1,15 +1,20 @@
 properties {
     $buildDir = ".\build"
-    $outputDir = $buildDir + "\lib\" + $framework
+    $outputDir = "$buildDir\lib\$framework"
+    $sourceDir = ".\Source\Amazon.WebServices.MechanicalTurk"
+    $resourceDir = "$sourceDir\Resources"
+    $resourceResponseFile = "$buildDir\resources.res"
 }
 
 task default -depends Compile, Clean
 
 task Compile -depends Clean { 
-    $sources = gci ".\CWS.Utility.SharePoint" -r -fi *.cs |% { $_.FullName }
+    $sources = gci $sourceDir -r -fi *.cs | select -expand FullName
+    gci $resourceDir |% { "/resource:$resourceDir\$_,Amazon.WebServices.MechanicalTurk.Resources.$_" } > $resourceResponseFile
     mkdir $outputDir
-    $out = $outputDir + "\CWS.Utility.SharePoint.dll"
-    csc /target:library /out:$out $sources 
+    $out = $outputDir + "\Amazon.WebServices.MechanicalTurk.dll"
+    csc /target:library /out:$out $sources "@$resourceResponseFile"
+    ri $resourceResponseFile
 }
 
 task Clean { 
